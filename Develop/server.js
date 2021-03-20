38c32aa372dd9c714f8b01c26f4824de10089161
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-03-19 23:24:47 -0700

    friday night jamjam

--- Develop/server.js
+++ Develop/server.js
@@ -1,40 +1,54 @@
-//dependencies
+// Dependencies
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const dbJSON = require("./db/db.json");
-
-//uuid package installed for unique id for each note
 const { v4: uuidv4 } = require('uuid');
-
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3005;
 
-//sets up express app to handle data parsing
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./public'));
 
-//routes to notes html
+//ROUTES 
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html')));
+
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-//routes to index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-//reads and returns all saved notes as JSON
+//  READ THE DB.JSON FILE AND RETURN SAVED NOTES AS JSON
 app.get('/api/notes', function (req, res) {
     fs.readFile('./db/db.json', 'utf8', function (err, data) {
         res.json(JSON.parse(data));
     })
 });
 
+//WRITING NOTES 
+app.post("/api/notes", function (req, res) {
+    const notes = req.body;
+    notes.id = uuidv4();
+    dbJSON.push(notes);
+    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(dbJSON, null, 2), (err) => {
+        if (err) {
+            return res.json({ error: "Error writing to file" });
+        }
+        return res.json(newNote);
+    });
+});
 
-//starts server to begin listening
+// Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+
+
+
+
+
+
 
 
 
